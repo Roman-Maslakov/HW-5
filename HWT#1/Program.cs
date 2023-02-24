06f23f6c47a922a54136724f9463495b43f81cbb
Ramses <rexid44@gmail.com>
2023-02-24 22:49:34 +1000

    1 % 2 done

--- HWT#1/Program.cs
+++ HWT#1/Program.cs
@@ -2,3 +2,31 @@
 Напишите программу, которая покажет количество чётных чисел в массиве.
 [345, 897, 568, 234] -> 2 */
 
+//Console.WriteLine("Quantity of even numbers in created array is - " + Sort(CreateArray()));
+int[] MyArr = CreateArray();
+string array = String.Join(", ", MyArr);
+int S = Sort(MyArr);
+Console.WriteLine("[" + array + "] -> " + S);
+Console.WriteLine("Sum of odd numbers in created array is - " + S);
+
+int Fill()
+{
+    int n = new Random().Next(100, 1000);
+    return n;
+}
+
+int Sort(int[] arr)
+{
+    int sort = 0;
+    for (int i = 0; i < arr.Length; i++) 
+        if (arr[i] % 2 == 0) sort++;
+    return sort;
+}
+
+int[] CreateArray()
+{
+    int i = new Random().Next(4, 11); //в условии не указано кол-во элементов массива
+    int[] Arr = new int[i];
+    for (int j = 0; j < Arr.Length; j++) Arr[j] = Fill();
+    return Arr;
+}
--- HWT#2/Program.cs
+++ HWT#2/Program.cs
@@ -3,3 +3,29 @@
 [3, 7, 23, 12] -> 19
 [-4, -6, 89, 6] -> 0 */
 
+int[] MyArr = CreateArray();
+string array = String.Join(", ", MyArr);
+int SON = SumOfOdd(MyArr);
+Console.WriteLine("[" + array + "] -> " + SON);
+Console.WriteLine("Sum of odd numbers in created array is - " + SON);
+
+int Fill()
+{
+    int n = new Random().Next(-99, 100);
+    return n;
+}
+
+int[] CreateArray()
+{
+    int i = new Random().Next(4, 11); //в условии не указано кол-во элементов массива
+    int[] Arr = new int[i];
+    for (int j = 0; j < Arr.Length; j++) Arr[j] = Fill();
+    return Arr;
+}
+
+int SumOfOdd(int[] arr)
+{
+    int Sodd = 0;
+    for (int i = 1; i < arr.Length; i += 2) Sodd += arr[i];
+    return Sodd;
+}
