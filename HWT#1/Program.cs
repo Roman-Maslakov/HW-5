916daffe8eee6302f459471e6104d19ea8fe752d
Ramses <rexid44@gmail.com>
2023-03-04 17:35:15 +1000

    done

--- HWT#1/Program.cs
+++ HWT#1/Program.cs
@@ -1,15 +1,31 @@
-﻿static int[,] CreateDoubleArray(int m, int n)
+﻿/* Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9 */
+
+Console.WriteLine("Создайте массив размером m*n");
+Console.Write("Введите m = ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n = ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+PrintDuoArray(CreateDoubleArray(m, n));
+
+double[,] CreateDoubleArray(int x, int y)
 {
-    int[,] arrayDuo = new int[m, n];
+    double[,] arrayDuo = new double[x, y];
+    int maxNumber = 10; //ввел диапозон
+    int minNumber = -10;
     for (int i = 0; i < arrayDuo.GetLength(0); i++)
     {
         for (int j = 0; j < arrayDuo.GetLength(1); j++)
-            arrayDuo[i, j] = new Random().Next(2, 10);
+            arrayDuo[i, j] = Math.Round(new Random().NextDouble() * (maxNumber - minNumber) + minNumber, 1);
     }
     return arrayDuo;
 }
 
-static void PrintDuoArray(int[,] duoArr)
+void PrintDuoArray(double[,] duoArr)
 {
     for (int i = 0; i < duoArr.GetLength(0); i++)
     {
